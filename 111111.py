02f7a23df8abdafbae59e1c99fd6bfe0f12fd1df
RyanL <2463704623@qq.com>
2022-07-18 14:15:53 +0800

    +测试

--- 111111.py
+++ 111111.py
@@ -18,10 +18,10 @@ def run():
     # os.system("python ./4_wechat11.py")
     # sleep(5)
 
-    print('现在要开始发tableau每日汇报，不要动鼠标喔')
-    os.system("python ./每日汇报发送微信.py")
-    sleep(3)
-
+    # print('现在要开始发tableau每日汇报，不要动鼠标喔')
+    # # os.system("python ./每日汇报发送微信.py")
+    # sleep(3)
+    #
     print('现在开始更新共享盘文件啦！！！')
     os.system("python ./日常更新共享盘文件.py")
     sleep(3)
